c49ea2dbfd3056a8764f0f245f5050102d7e57e3
manhtienmai <mtmanh04@gmail.com>
2025-02-21 18:57:50 +0700

    add frontend part to repo

---
+++ hotel-booking-frontend/src/components/layout/Header.tsx
@@ -0,0 +1,7 @@
+const Header = () => (
+    <div className="bg-blue-800 p-4">
+        <span className="text-white text-2xl font-bold cursor-pointer">
+            CozyBook.com
+        </span>
+    </div>
+);
